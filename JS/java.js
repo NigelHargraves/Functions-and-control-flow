10f20358014c21086fdff7f095da18e64fa23f55
Nigel Hargraves <nigelhargraves@ymail.com>
2021-06-21 13:06:05 +0100

    test

--- JS/java.js
+++ JS/java.js
@@ -1,33 +1,28 @@
+//function to calculate percentage
 function calcPercent(number, percent) {
     return (number / 100) * percent;
 }
 
-const result = calcPercent(135, 30);
-document.write(result, '<br>');
+const result = calcPercent(135, 30); //declare variable and call function
 
+document.write(result, '<br>'); //write result
+
+//function to switch drinkType
 function drinkOrder(size, type) {
-    switch (size, type) {
-        case "small", "cola":
-
-
-
+    switch (type) {
+        case "cola":
+            drinkType = 'Cola';
+            break;
+        case "lemon":
+            drinkType = 'Lemonade';
+            break;
+        case "orange":
+            drinkType = 'Orangeade';
     }
-
-
-
+    return `you have ordered a ${size} drink of ${drinkType}.`;
 }
-var drinkSize = '';
-var drinkType = '';
+//variables
+var drinkSize = 'large',
+    drinkType = 'lemon';
 
-function sizeval(varval) {
-    drinkSize = varval;
-    alert(drinkSize);
-}
-
-function typeval(varval) {
-    drinkType = varval;
-    alert(drinkType);
-}
-
-drinkOrder(drinkSize, drinkType);
-document.write(drinkSize);
+document.write(drinkOrder(drinkSize, drinkType)); //call function and pass parameters and write result
